104be573274d18d188467a4efa2ad10afedc235b
JP Dehollain <jpdehollain@github.com>
2017-11-09 13:34:27 +0100

    imported test functions into script.py

--- script.py
+++ script.py
@@ -1,1 +1,4 @@
-print('some print statement')
+from test import *
+
+print('the circumference of a circle of radius 3 is',circumference(3))
+print('the surface area of a circle of radius 4 is',circlearea(4))
